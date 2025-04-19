9402ba39760b44eccceb89d87694189d8aa87ef6
EC2 Default User <ec2-user@ip-172-31-34-30.ap-northeast-1.compute.internal>
2025-04-19 14:09:32 +0000

    [Finish]通知機能

--- config/routes.rb
+++ config/routes.rb
@@ -21,4 +21,5 @@ Rails.application.routes.draw do
   
   get "search" => "searches#search"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :notifications, only: [:update]
 end
