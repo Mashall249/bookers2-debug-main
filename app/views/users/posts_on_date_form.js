09e29b90636199a0de37d241265c2ff8e41e07f0
EC2 Default User <ec2-user@ip-172-31-34-30.ap-northeast-1.compute.internal>
2025-04-23 08:00:39 +0000

    [Add]Favoriteソート

---
+++ app/views/users/posts_on_date_form.js
@@ -0,0 +1,9 @@
+$('.posts_on_date_table').html(`
+  <h4>検索結果</h4>
+  <div class="card" >
+    <div class="card-header">投稿数</div>
+    <div class="card-body">
+      <%= j( @books.count ) %>
+    </div>
+  </div>
+`)
